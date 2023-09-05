751e8685d7c2502eed4fcfa4df83b34c1c178740
Ahmed-Nasser-99 <forall3020@gmail.com>
2023-09-05 23:21:35 +0300

    Introduce ResourceAvailabilityContainer component

---
+++ components/manageResource/ResourceAvailability/ResourceAvailabilityContainer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ResourceAvailabilityContainer = () => {
+  return (
+    <div className="flex flex-col justify-center items-center bg-[#FAF9F9] py-2 px5 w-full">
+      ResourceAvailabilityContainer
+    </div>
+  );
+};
+
+export default ResourceAvailabilityContainer;
